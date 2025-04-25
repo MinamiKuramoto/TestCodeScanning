928cc4771192dfccf69ef3d8ebaf0462ec06065f
Kuramoto Minami <minami_kuramoto@jp.ricoh.com>
2025-04-25 11:46:13 +0900

    consoleアプリ

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+string userInput = "aaaa";
+
+// 脆弱性：SQLインジェクション
+string query = "SELECT * FROM Users WHERE username = '" + userInput + "'";
+
+using (SqlConnection connection = new SqlConnection("Data Source=localhost;Initial Catalog=MyDb;Integrated Security=True"))
+{
+    SqlCommand command = new SqlCommand(query, connection);
+    connection.Open();
+    SqlDataReader reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        Console.WriteLine($"ユーザーID: {reader["id"]}, 名前: {reader["username"]}");
+    }
+}
